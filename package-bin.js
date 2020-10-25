5ab245b183d69b4c97d8e10d3da6f9ce992828ee
Rosano Coutinho <pub@xeari.com>
2020-10-25 11:50:42 -0300

    Scaffold bin

---
+++ package-bin.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const mod = {
+
+	// VALUE
+
+	_ValueDirectory: process.cwd(),
+
+	// CONTROL
+
+	ControlCompile(args) {
+		require('./main.js').OLSKInternationalWriteCompilationFile({
+			OLSKInternationalFileDelegateGlobSync: require('glob').sync,
+			OLSKInternationalFileDelegateYAMLRead: require('js-yaml').safeLoad,
+		}, mod._ValueDirectory);
+	},
+
+	// SETUP
+
+	SetupEverything () {
+		mod.SetupDirectory();
+
+		if (process.argv[1].endsWith('olsk-international-compile')) {
+			return mod.ControlCompile(process.argv.slice(2));
+		}
+	},
+
+	SetupDirectory () {
+		if (process.argv.length !== 2) {
+			mod._ValueDirectory = process.argv.slice(-1).pop();
+		}
+	},
+
+	// LIFECYCLE
+
+	LifecycleScriptDidLoad() {
+		mod.SetupEverything();
+	},
+
+};
+
+mod.LifecycleScriptDidLoad();
